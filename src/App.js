c5069378874ae86f8aaa57f2c81b100518bde9e1
Sonam Chhogyel <sonam.chhogyel@empired.com>
2017-12-15 12:53:41 +1000

    Add annotorious method and event handlers

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    annotations: []
+  };
+  annoInit = () => {
+    window.anno.makeAnnotatable(this.myImage);
+  };
+  annoHandler = (handler, fn) => {
+    window.anno.addHandler(handler, fn);
+  };
+  createAnnotation = annotation => {
+    this.setState({
+      annotations: [...this.state.annotations, annotation]
+    });
+  };
+  componentDidMount() {
+    this.annoInit();
+    this.annoHandler("onAnnotationCreated", this.createAnnotation);
+  }
+  componentWillUnmount() {
+    window.anno.destroy();
+  }
   render() {
     return (
       <div className="App">
         <div className="App-image__container">
-          <img className="App-image" src="./pexels-photo-133394.jpeg" />
+          <img
+            ref={r => (this.myImage = r)}
+            className="App-image"
+            src="./pexels-photo-133394.jpeg"
+            alt=""
+          />
         </div>
       </div>
     );
